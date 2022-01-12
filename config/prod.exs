20c298a7203c96655a992570ece6f3998739ae21
amalbuquerque <andre.malbuq@gmail.com>
2022-01-13 00:52:56 +0100

    Nginx + Docker-compose prod working!
    I had to follow other instructions since the ones I followed
    didn't work.
    
    Followed this:
    - https://elixirforum.com/t/nginx-reverse-proxy-with-phoenix-and-lets-encrypt-ssl-config/33463/3
    - https://github.com/derek-zhou/liv (webmail app with the Nginx
    settings)

--- config/prod.exs
+++ config/prod.exs
@@ -10,11 +10,15 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :andrex, AndrexWeb.Endpoint,
-  http: [port: 4000],
+  http: [
+    ip: {127, 0, 0, 1},
+    port: 4000
+  ],
   debug_errors: true,
   check_origin: false,
   url: [host: "a14e.eu", port: 443, scheme: "https"],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :debug
